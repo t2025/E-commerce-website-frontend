98dee112eafb24271400ac6f8a5b5342e5e43e06
Tanvi Ranga <Tanvi Ranga>
2017-04-10 00:33:54 +0530

    third

--- script.js
+++ script.js
@@ -12,28 +12,30 @@ $(document).ready(function(){
       var data=response["data"];
       data.forEach(function(product,index){
         var element="<div class='product-container col-md-4 col-xs-12'></div>";
+        var thing="<div class='purchasing-item>'</div>";
         var productDiv=$("<div class='productcontainer'></div>").text(product["item_name"]);
         var img=$("<img>").attr("src",product["image"]);
         var brandName=$("<div class='brand-name'></div>").text(product["brand"]);
-        var icon=  "<div class='icon'><i class='fa fa-shopping-cart' aria-hidden='true'></i></div>"
-        var desc=$("<div class='des'></div>").text(product["description"]);
+      var desc=$("<div class='des'></div>").text(product["description"]);
         var costDiv=$("<div class='cost'></div>").text(product["price"]);
         var sellerDiv=$("<div class='seller'></div>").text(product["sold_by"]);
         var quantity=$("<div class='number'></div>").text(product["quantity"]);
+        var icon=$("<div class='icon'><i class='fa fa-shopping-cart fa' aria-hidden='true'></i> </div>");
+
+        element=$(element).append(img,productDiv,brandName,desc,costDiv,sellerDiv,quantity,icon);
 
 
-        element=$(element).append(img,productDiv,brandName,icon,desc,costDiv,sellerDiv,quantity);
         $('.product').append(element);
         $(".productcontainer").click(function() {
-          var productName=$($(this).find(".productcontainer")).text();
+          var productName=$($(this).parent().find(".productcontainer")).text();
           console.log(productName);
-          var imageSource=$($(this).find("img")).attr("src");
+          var imageSource=$($(this).parent().find("img")).attr("src");
           console.log(imageSource);
-          var brandName=$($(this).find(".brand-name")).text();
-          var desr=$($(this).find(".des")).text();
-          var price=$($(this).find(".cost")).text();
-            var sell=$($(this).find(".seller")).text();
-              var num=$($(this).find(".number")).text();
+          var brandName=$($(this).parent().find(".brand-name")).text();
+          var desr=$($(this).parent().find(".des")).text();
+          var price=$($(this).parent().find(".cost")).text();
+            var sell=$($(this).parent().find(".seller")).text();
+              var num=$($(this).parent().find(".number")).text();
           localStorage.setItem("productName",productName);
           localStorage.setItem("image",imageSource);
 
@@ -44,21 +46,53 @@ $(document).ready(function(){
           localStorage.setItem("quantity",num);
           window.location.href="product.html";
 
-        $(".icon").click(function() {
-            var productName=$($(this).find(".productcontainer")).text();
-            console.log(productName);
-            var num=$($(this).find(".number")).text();
-            var price=$($(this).find(".cost")).text();
-            $("product-list").append(productName);
-        });
+
 
         });
 
 
 
 
-      });
-    }
+
+
   });
+  $(".icon").click(function(){
+    var productName=$($(this).parent().find(".productcontainer")).text();
+        var price=$($(this).parent().find(".cost")).text();
+          var num=$($(this).parent().find(".number")).text();
+
+    var list="<li></li>";
+    var costlist="<li></li>";
+    var numlist="<li></li>";
+    list=$(list).append(productName);
+    costlist=$(costlist).append(price);
+    numlist=$(numlist).append(num);
+    $("#product-list").append(list);
+    $("#cost-list").append(costlist);
+    $("#quant-list").append(numlist);
+alert('Item has been added to cart');
+  });
+
+
+
+
+}
+});
+$.ajax({
+  url: "http://acadprojects.com/py/fabricKart/sell",
+  type:'POST',
+  data: {
+
+  }
+
+});
+$.ajax({
+  url: "http://acadprojects.com/py/fabricKart/checkout",
+  type:'PUT',
+  data: {
+
+  }
+
+});
 
 });
