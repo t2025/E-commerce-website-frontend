b6847e68262403714f906715fe514c9d107d993e
Tanvi Ranga <Tanvi Ranga>
2017-04-10 20:03:50 +0530

    Fourth commit

--- script.js
+++ script.js
@@ -12,30 +12,30 @@ $(document).ready(function(){
       var data=response["data"];
       data.forEach(function(product,index){
         var element="<div class='product-container col-md-4 col-xs-12'></div>";
-        var thing="<div class='purchasing-item>'</div>";
+
         var productDiv=$("<div class='productcontainer'></div>").text(product["item_name"]);
         var img=$("<img>").attr("src",product["image"]);
         var brandName=$("<div class='brand-name'></div>").text(product["brand"]);
       var desc=$("<div class='des'></div>").text(product["description"]);
         var costDiv=$("<div class='cost'></div>").text(product["price"]);
         var sellerDiv=$("<div class='seller'></div>").text(product["sold_by"]);
-        var quantity=$("<div class='number'></div>").text(product["quantity"]);
+        var productnum=$("<div class='number'></div>").text(product["quantity"]);
         var icon=$("<div class='icon'><i class='fa fa-shopping-cart fa' aria-hidden='true'></i> </div>");
 
-        element=$(element).append(img,productDiv,brandName,desc,costDiv,sellerDiv,quantity,icon);
+        element=$(element).append(img,productDiv,brandName,desc,costDiv,sellerDiv,productnum,icon);
 
 
         $('.product').append(element);
         $(".productcontainer").click(function() {
           var productName=$($(this).parent().find(".productcontainer")).text();
-          console.log(productName);
+
           var imageSource=$($(this).parent().find("img")).attr("src");
-          console.log(imageSource);
+
           var brandName=$($(this).parent().find(".brand-name")).text();
           var desr=$($(this).parent().find(".des")).text();
           var price=$($(this).parent().find(".cost")).text();
-            var sell=$($(this).parent().find(".seller")).text();
-              var num=$($(this).parent().find(".number")).text();
+          var sell=$($(this).parent().find(".seller")).text();
+          var num=$($(this).parent().find(".number")).text();
           localStorage.setItem("productName",productName);
           localStorage.setItem("image",imageSource);
 
@@ -43,7 +43,7 @@ $(document).ready(function(){
           localStorage.setItem("description",desr);
           localStorage.setItem("pcost",price);
           localStorage.setItem("soldby",sell);
-          localStorage.setItem("quantity",num);
+          localStorage.setItem("itemnumber",num);
           window.location.href="product.html";
 
 
@@ -70,7 +70,7 @@ $(document).ready(function(){
     $("#product-list").append(list);
     $("#cost-list").append(costlist);
     $("#quant-list").append(numlist);
-alert('Item has been added to cart');
+    alert('Item has been added to cart');
   });
 
 
