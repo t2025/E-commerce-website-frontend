b1e5e970aa277d67796732b9567edeedfd7fbcd9
Tanvi Ranga <Tanvi Ranga>
2017-04-13 15:27:54 +0530

    commit

--- script.js
+++ script.js
@@ -10,14 +10,10 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
-
 //sell form event
-
 $("#sell").click(function(){
   $("#sell-myntra").show();
 });
-
-
 $("#cartid").click(function(){
   $("#cart").show();
 });
@@ -36,8 +32,7 @@ function validateform()
   console.log(x);
   var y=document.forms["cartform"]["email"].value;
   var z=document.forms["cartform"]["number"].value;
-
-  if(x==""){
+    if(x==""){
   alert("All fields are mandatory");
   return false;
 }
@@ -60,7 +55,6 @@ function validatecartform()
   var discountvar=document.forms["Sellform"]["discount"].value;
   var pricevar=document.forms["Sellform"]["price"].value;
   var sellvar=document.forms["Sellform"]["seller"].value;
-
   if(prname==""){
   alert("All fields are mandatory");
   return false;
@@ -86,11 +80,6 @@ alert("All fields are mandatory");
 return false;
 }
 }
-
-
-
-
-
 // sort api call
 $(document).ready(function(){
   $.ajax({
@@ -101,11 +90,10 @@ $(document).ready(function(){
       "page": 0,
     },
     success:function(response){
-      console.log(response);
-      var data=response["data"];
-      data.forEach(function(product,index){
+        console.log(response);
+        var data=response["data"];
+        data.forEach(function(product,index){
         var element="<div class='product-container col-md-4 col-xs-12'></div>";
-
         var productDiv=$("<div class='productcontainer'></div>").text(product["item_name"]);
         var img=$("<img>").attr("src",product["image"]);
         var brandName=$("<div class='brand-name'></div>").text(product["brand"]);
@@ -114,16 +102,11 @@ $(document).ready(function(){
         var sellerDiv=$("<div class='seller'></div>").text(product["sold_by"]);
         var productnum=$("<div class='number'></div>").text(product["quantity"]);
         var icon=$("<div class='icon'><i class='fa fa-shopping-cart fa' aria-hidden='true'></i> </div>");
-
         element=$(element).append(img,productDiv,brandName,desc,costDiv,sellerDiv,productnum,icon);
-
-
-        $('.product').append(element);
+  $('.product').append(element);
         $(".productcontainer").click(function() {
           var productName=$($(this).parent().find(".productcontainer")).text();
-
           var imageSource=$($(this).parent().find("img")).attr("src");
-
           var brandName=$($(this).parent().find(".brand-name")).text();
           var desr=$($(this).parent().find(".des")).text();
           var price=$($(this).parent().find(".cost")).text();
@@ -131,41 +114,32 @@ $(document).ready(function(){
           var num=$($(this).parent().find(".number")).text();
           localStorage.setItem("productName",productName);
           localStorage.setItem("image",imageSource);
-
           localStorage.setItem("brandname",brandName);
           localStorage.setItem("description",desr);
           localStorage.setItem("pcost",price);
           localStorage.setItem("soldby",sell);
           localStorage.setItem("itemnumber",num);
           window.location.href="product.html";
-
-
-
-        });
-
-
+  });
 });
 }
-
-
-
-  });
-  $(".product-name").append(localStorage.getItem("productName"));
+});
+//product page appending items
+$(".product-name").append(localStorage.getItem("productName"));
   var image=$("<img>").attr("src",localStorage.getItem("image"));
-  $(".image").append(image);
-  $(".brandname").append(localStorage.getItem("brandname"));
-  $(".descr").append(localStorage.getItem("description"));
-  $(".soldby").append(localStorage.getItem("soldby"));
-  $(".quant").append(localStorage.getItem("itemnumber"));
-  $(".price").append(localStorage.getItem("pcost"));
-  $("#cartbutton").click(function(){
+$(".image").append(image);
+$(".brandname").append(localStorage.getItem("brandname"));
+$(".descr").append(localStorage.getItem("description"));
+$(".soldby").append(localStorage.getItem("soldby"));
+$(".quant").append(localStorage.getItem("itemnumber"));
+$(".price").append(localStorage.getItem("pcost"));
+$("#cartbutton").click(function(){
   alert('The item has been added to cart');
   });
   $(".icon").click(function(){
     var productName=$($(this).parent().find(".productcontainer")).text();
-        var price=$($(this).parent().find(".cost")).text();
-          var num=$($(this).parent().find(".number")).text();
-
+    var price=$($(this).parent().find(".cost")).text();
+    var num=$($(this).parent().find(".number")).text();
     var list="<li></li>";
     var costlist="<li></li>";
     var numlist="<li></li>";
@@ -177,16 +151,11 @@ $(document).ready(function(){
     $("#quant-list").append(numlist);
     alert('Item has been added to cart');
   });
-
-
-
-
-
-
+  //filter items
 $("#searchbutton").click(function(){
 var searchquery= $("#search").val();
 $(".container").empty();
-
+//filter ajax call
 $.ajax({
   url:"http://acadprojects.com/py/fabricKart/filter/items",
   type: "POST",
@@ -205,70 +174,47 @@ $.ajax({
 success: function(response) {
   console.log(response);
   $("#sell-myntra").show();
-
-
-   var data=response["data"];
-console.log("data");
-
+  var data=response["data"];
+   console.log("data");
    data.forEach(function(product,index){
-      var element="<div class='product-container col-md-4 col-xs-12'></div>";
-
-
-      var productDiv=$("<div class='productcontainer'></div>").text(product["item_name"]);
-      console.log(productDiv);
-      var img=$("<img>").attr("src",product["image"]);
-      console.log(img);
-      var brandName=$("<div class='brand-name'></div>").text(product["brand"]);
-      var desc=$("<div class='des'></div>").text(product["description"]);
-      var costDiv=$("<div class='cost'></div>").text(product["price"]);
-      var sellerDiv=$("<div class='seller'></div>").text(product["sold_by"]);
-      var productnum=$("<div class='number'></div>").text(product["quantity"]);
-      var icon=$("<div class='icon'><i class='fa fa-shopping-cart fa' aria-hidden='true'></i> </div>");
-
-      element=$(element).append(img,productDiv,brandName,desc,costDiv,sellerDiv,productnum,icon);
-
-
-      $('.brandfilter').append(element);
-      $(".productcontainer").click(function() {
-        var productName=$($(this).parent().find(".productcontainer")).text();
-
-        var imageSource=$($(this).parent().find("img")).attr("src");
-
-        var brandName=$($(this).parent().find(".brand-name")).text();
-
-        var desr=$($(this).parent().find(".des")).text();
-        var price=$($(this).parent().find(".cost")).text();
-        var sell=$($(this).parent().find(".seller")).text();
-        var num=$($(this).parent().find(".number")).text();
-        localStorage.setItem("productName",productName);
-        localStorage.setItem("image",imageSource);
-
-        localStorage.setItem("brandname",brandName);
-        localStorage.setItem("description",desr);
-        localStorage.setItem("pcost",price);
-        localStorage.setItem("soldby",sell);
-        localStorage.setItem("itemnumber",num);
-        window.location.href="product.html";
-
-
-
-      });
-
-
-
-
+   var element="<div class='product-container col-md-4 col-xs-12'></div>";
+   var productDiv=$("<div class='productcontainer'></div>").text(product["item_name"]);
+  console.log(productDiv);
+  var img=$("<img>").attr("src",product["image"]);
+  console.log(img);
+  var brandName=$("<div class='brand-name'></div>").text(product["brand"]);
+  var desc=$("<div class='des'></div>").text(product["description"]);
+  var costDiv=$("<div class='cost'></div>").text(product["price"]);
+  var sellerDiv=$("<div class='seller'></div>").text(product["sold_by"]);
+  var productnum=$("<div class='number'></div>").text(product["quantity"]);
+  var icon=$("<div class='icon'><i class='fa fa-shopping-cart fa' aria-hidden='true'></i> </div>");
+  element=$(element).append(img,productDiv,brandName,desc,costDiv,sellerDiv,productnum,icon);
+  $('.product').append(element);
+   $(".productcontainer").click(function() {
+    var productName=$($(this).parent().find(".productcontainer")).text();
+    var imageSource=$($(this).parent().find("img")).attr("src");
+    var brandName=$($(this).parent().find(".brand-name")).text();
+    var desr=$($(this).parent().find(".des")).text();
+    var price=$($(this).parent().find(".cost")).text();
+    var sell=$($(this).parent().find(".seller")).text();
+    var num=$($(this).parent().find(".number")).text();
+    localStorage.setItem("productName",productName);
+    localStorage.setItem("image",imageSource);
+    localStorage.setItem("brandname",brandName);
+    localStorage.setItem("description",desr);
+    localStorage.setItem("pcost",price);
+    localStorage.setItem("soldby",sell);
+    localStorage.setItem("itemnumber",num);
+    window.location.href="product.html";
+    });
   });
 }
 });
 });
 });
-
-
+//sell api call
 $("#sellbutton").click(function(event) {
   event.preventDefault();
-
-
-
   var namefield= $("#pname").val();
   var brandfield=$("#bname").val();
   var categoryfield=$("#category").val();
@@ -276,18 +222,16 @@ $("#sellbutton").click(function(event) {
   var discountfield=$("#discount").val();
   var descrfield=$("#description").val();
   var sellerfield=$("#seller").val();
-
-  $.ajax({
+$.ajax({
     url: "http://acadprojects.com/py/fabricKart/sell",
     type: "POST",
     beforeSend: function(xhr) {
       xhr.setRequestHeader("Content-type", "application/json");
     },
-
     data : JSON.stringify({
-      "item_name" : namefield,
-      "brand": brandfield,
-      "size": "42",
+    "item_name" : namefield,
+    "brand": brandfield,
+    "size": "42",
     "price": pricefield,
     "sold_by": sellerfield,
     "quantity": "22",
@@ -298,11 +242,9 @@ $("#sellbutton").click(function(event) {
     "brand": brandfield,
     "description": descrfield
   }),
-
-    success:function(response){
+      success:function(response){
       console.log(response);
     }
-
 });
 alert("sold");
 });
